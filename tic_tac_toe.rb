950e4076ae098c083169e7867a2b3d91d28a5c2e
Joel Shapiro <joeladamshapiro@gmail.com>
2022-09-20 21:25:33 -0400

    initial commit

---
+++ tic_tac_toe.rb
@@ -0,0 +1,8 @@
+#PseudoCode
+#Create a grid of 9 squares
+#Player One user input prompt (X)
+#Player Two user input prompt (O)
+#Develop a way for user to choose their grid position
+#Develop game logic to identify a winner
+#Notify the winner!
+#Option to play a new game
